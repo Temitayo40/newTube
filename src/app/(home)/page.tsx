e796e2b65a1194725d3c37b5df7205195315ad59
adebowale damilola <damilola.adebowale@abbaandking.com>
2025-09-08 16:24:29 +0100

    trpc configuration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { HydrateClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "damilola" });
   return (
-    <div className="text-2xl font-semibold text-rose-600">
-      I will load videos in the future
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}></ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
